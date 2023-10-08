6f8c47168c6a9bf8c00c10596d510aa9160b7e70
BambooDay <2433627306@qq.com>
2023-10-08 11:39:51 +0800

    none

--- code/模拟赛/20231008/ZJ-00050/flower/flower.cpp
+++ code/模拟赛/20231008/ZJ-00050/flower/flower.cpp
@@ -16,7 +16,7 @@ void solve(){
 	}
 	dp[1][1] = abs(a[1] - 1);
 	for(int i = 2; i <= n; i++){
-		for(int j = 1; j <= i;j++){
+		for(int j = 1; j <= min(i,m);j++){
 			dp[i][j] = min(dp[i-1][j],dp[i-1][j-1]) + abs(a[i] - j);
 		}
 	}	
